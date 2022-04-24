cdefb39e4c50a0fe9a401259759734de16b0da41
Bhavana Rana <4bhavanarana@gmail.com>
2022-04-24 14:34:59 +0530

    Completed navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-document.getElementById("para").innerHTML = "Ahaaan it's working darling";
+let navContent = document.getElementsByClassName("nav_home");//this will return an array of html element eg nav_home nav_home so on 
+let logoName = document.getElementById("logo_title");//sweat arena
+let logOut = document.getElementById("log_out");//slidebar icon
+let sideBar =document.getElementById("showSidebar");
+var clicked=true;
+logOut.onclick = function(){
+    if(clicked==true){  
+        clicked=false;
+        sideBar.style.width = "290px";
+        sideBar.style.transition = "0.8s ease";
+        logOut.style.transform= "rotate(180deg)";
+        logoName.style.display = "block";
+        for (var i = 0; i < navContent.length; i++){     // to access every element of array we use loop
+            navContent[i].style.display = 'inline-block';
+          }
+    }
+    else{
+        clicked=true;
+        sideBar.style.width = "90px";
+        sideBar.style.transition = "0.8s ease";
+        logOut.style.transform= "rotate(360deg)";
+        logOut.style.right= "0";
+        logoName.style.display = "none";
+        for (var i = 0; i < navContent.length; i += 1){
+            navContent[i].style.display = 'none';
+    }
+}
+}
