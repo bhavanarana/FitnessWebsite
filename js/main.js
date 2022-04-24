92331be000e880dcce57169cc4f044f4ed116562
Bhavana Rana <4bhavanarana@gmail.com>
2022-04-24 15:41:33 +0530

    added smooth transition  navbar

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
-let navContent = document.getElementsByClassName("nav_home");//this will return an array of html element eg nav_home nav_home so on 
-let logoName = document.getElementById("logo_title");//sweat arena
-let logOut = document.getElementById("log_out");//slidebar icon
-let sideBar =document.getElementById("showSidebar");
-var clicked=true;
-logOut.onclick = function(){
-    if(clicked==true){  
-        clicked=false;
-        sideBar.style.width = "290px";
-        sideBar.style.transition = "0.8s ease";
-        logOut.style.transform= "rotate(180deg)";
-        logoName.style.display = "block";
-        for (var i = 0; i < navContent.length; i++){     // to access every element of array we use loop
-            navContent[i].style.display = 'inline-block';
-          }
-    }
-    else{
-        clicked=true;
-        sideBar.style.width = "90px";
-        sideBar.style.transition = "0.8s ease";
-        logOut.style.transform= "rotate(360deg)";
-        logOut.style.right= "0";
-        logoName.style.display = "none";
-        for (var i = 0; i < navContent.length; i += 1){
-            navContent[i].style.display = 'none';
-    }
+let navContent = document.getElementsByClassName("nav_home"); //this will return an array of html element eg nav_home nav_home so on
+let logoName = document.getElementById("logo_title"); //sweat arena
+let logOut = document.getElementById("log_out"); //slidebar icon
+let logIn = document.getElementById("log_in");
+let sideBar = document.getElementById("showSidebar");
+
+function smooth_transition() {
+  for (var i = 0; i < navContent.length; i++) {
+    // to access every element of array we use loop
+    navContent[i].style.display = "inline-block";
+    logoName.style.display = "block";
+  }
 }
-}
+logOut.onclick = function () {
+  sideBar.style.width = "290px";
+  sideBar.style.transition = "0.3s ease";
+  logOut.style.display = "none";
+  logIn.style.display = "block";
+  setTimeout(smooth_transition, 300); //setTimeout func runs a code after certain time in millisecond
+};
+logIn.onclick = function () {
+  sideBar.style.width = "90px";
+  sideBar.style.transition = "0.8s ease";
+  logOut.style.display = "block";
+  logoName.style.display = "none";
+  logIn.style.display = "none";
+  for (var i = 0; i < navContent.length; i += 1) {
+    navContent[i].style.display = "none";
+  }
+};
