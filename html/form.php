c1a5a8bb2f54127ee5de3b0f736bb21f806cf78b
Bhavana Rana <4bhavanarana@gmail.com>
2022-06-05 23:15:17 +0530

    fromat doc

--- html/form.php
+++ html/form.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- <?php 
- include "bootstrap.php"
- ?>
+ <?php include 'bootstrap.php'; ?>
 </head>
 <body>
 <?php include 'navbar.php'; ?>
@@ -63,12 +61,11 @@
   <div class="col-12">
     <button type="submit" class="btn form-btn mt-2" >Submit</button>
   </div>
-</form> 
+</form>
       </div>
     </div>
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   <script src="../js/main.js"></script>
 </body>
-
 </html>
