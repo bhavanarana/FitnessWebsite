186b872216eaa0406920bf8a22e4f66d24fa372b
Bhavana Rana <4bhavanarana@gmail.com>
2022-06-07 12:40:55 +0530

    resolve errors

--- html/db.php
+++ html/db.php
@@ -7,18 +7,23 @@ $conn = new mysqli($servername, $username, $password, $db, '3307');
 if (!$conn) {
     die('failed' . mysqli_connect_error());
 }
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $email = $_POST['email'];
     $name = $_POST['name'];
     $goal = $_POST['goal'];
     $workout_lvl = $_POST['workout_lvl'];
     $diet = $_POST['diet'];
-    $dob = $_POST['DOB'];
+    $dob = $_POST['date'];
     $height = $_POST['height'];
     $weight = $_POST['weight'];
-    $blood_grp = $_POST['blood_grp'];
-    $query  = INSERT INTO record(Email, Name, Goal,	Workout_lvl, Diet, DOB,	Height,	Weight,	Blood_Group) 
-    VALUES('$email', '$name', '$goal', '$wrokout_lvl', '$diet', '$dob', '$height', '$weight', '$blood_group');
+    $blood_grp = $_POST['blood-grp'];
+    $query = "INSERT INTO record(Email, Name, Goal, Workout_lvl, Diet, DOB, Height, Weight, Blood_Group)
+    VALUES('$email', '$name', '$goal', '$workout_lvl', '$diet', '$dob', '$height', '$weight', '$blood_grp')";
     $insert_query = mysqli_query($conn, $query);
+    if ($insert_query) {
+        echo 'Suceessfully submitted';
+    } else {
+        echo 'Failed to Submitted';
+    }
 }
 ?>
